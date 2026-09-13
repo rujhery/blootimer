8f02e50065ea7c9e11d4b9fc87a5f2ac653a7bd7
rujhery <nasayuzaki.code@gmail.com>
2026-09-12 20:51:28 -0500

    Fourth commit

--- src/main.py
+++ src/main.py
@@ -4,12 +4,19 @@ import os
 
 time1 = 25
 
-try: 
-    for i in range(time1, 0, -1):
-        os.system('clear')
-        print(f"left {i} minutes", end="\r")
-        time.sleep(60)
-    print(f"\nPomodor terminado")
+def main():
 
-except KeyboardInterrupt:
+    try: 
+        for i in range(time1, 0, -1):
+            os.system('clear')
+            print(f"left {i} minutes", end="\r")
+            time.sleep(60)
+        print(f"\nPomodor terminado")
+
+
+    except KeyboardInterrupt:
         print(f'\nCanceled, {time.ctime()}')
+
+if __name__ == "__main__":
+   main()
+
